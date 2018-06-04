94e6c4b36f11330612e5dbde7258d24b77d7144a
kyusonglee <kyusonglee@gmailcom>
2018-06-04 15:56:18 -0400

    update

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
-
 import Review from './Review';
+import { sendMessage } from '../actions/messageActions';
 
 class Message extends React.Component {
     render() {
@@ -16,7 +16,7 @@ class Message extends React.Component {
             </div>
             { this.props.received ? <Review index={ this.props.index }/> : null }
 	 		{this.props.candidate.length > 0 ? this.props.candidate.map((item,index) => (
-			  	 <button>{item}</button>
+			  	 <button onClick={()=>this.props.onEnter(item,Date.now())}  >{item}</button>
 			      )):null}
         </div>    
     }
@@ -26,7 +26,11 @@ function mapStateToProps(state, props) {
     return state.messages[props.index];
 }
 
-const mapDispatchToProps = {}
+
+const mapDispatchToProps = {
+    onEnter: sendMessage,
+}
+
 
 export default connect(mapStateToProps, mapDispatchToProps)(Message)
 
