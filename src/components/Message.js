038d7019157f55d3aa913bc7077dad20abf45ff1
kyusonglee <kyusonglee@gmailcom>
2018-06-04 13:48:14 -0400

    update

--- src/components/Message.js
+++ src/components/Message.js
@@ -10,7 +10,7 @@ class Message extends React.Component {
                 className={ 
                     (this.props.received ? "received" : "sent") + " message" 
                 }>
-			    {this.props.received ? <span className="messageText">{this.props.text}</span>: <span className="messageText">{this.props.text}</span>}
+			    {this.props.received ? <span className="messageText">{JSON.parse(this.props.text)["value"]}</span>: <span className="messageText">{this.props.text}</span>}
 				<img style={{"width":"250px"}} src="https://skylar.speech.cs.cmu.edu/~kyusonglee/website/assets/img/cmu.png"/>
             </div>
             { this.props.received ? <Review index={ this.props.index }/> : null }
